31235a46161961b5982c73dd83de8efc4d8aef0a
Neonnaut <Neonnaut25@gmail.com>
2022-11-09 06:41:53 +1100

    removed google client stuff

--- chessbot/__init__.py
+++ chessbot/__init__.py
@@ -3,15 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+TOKEN = os.getenv("TOKEN")
+
 CHECK = '<:check:1001706162665832448>'
 ERR = '<:err:1001706158786101269>'
 WARN = '<:warn:1001706160438657109>'
 INFO = '<:info:1003510951515005008>'
-
-TOKEN = os.getenv("TOKEN")
-
-GOOGLE_CLIENT = {
-    "private_key": os.getenv("PRIVATE_KEY"),
-    "client_email": os.getenv("CLIENT_EMAIL"),
-    "token_uri": os.getenv("TOKEN_URI"),
-}
