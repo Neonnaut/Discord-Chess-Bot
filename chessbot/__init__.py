67c3a0a391581cc8d8fa1282e7647b7a92ce6005
Neonnaut <Neonnaut25@gmail.com>
2022-11-09 17:14:53 +1100

    Update __init__.py

--- chessbot/__init__.py
+++ chessbot/__init__.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
-CHECK = '<:check:1001706162665832448>'
-ERR = '<:err:1001706158786101269>'
-WARN = '<:warn:1001706160438657109>'
-INFO = '<:info:1003510951515005008>'
+CHECK = ':white_check_mark:'
+ERR = ':x:'
+WARN = ':warning:'
+INFO = ':information_source:'
